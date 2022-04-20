65f37d3a24f2e87837756485a9e002269d3129e5
cantdrinkmilk <53961561+cantdrinkmilk@users.noreply.github.com>
2022-04-20 12:35:08 -0700

    Update MainMap.vue
    d

--- SafeRide/Frontend/src/components/MainMap.vue
+++ SafeRide/Frontend/src/components/MainMap.vue
@@ -86,13 +86,18 @@ export default {
   },
   props: ['api_key'],
   mounted () {
+<<<<<<< HEAD
     mapboxgl.accessToken = this.api_key
+=======
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2FudGRyaW5rbWlsayIsImEiOiJjbDAwZnFiOHkwM3kyM3FwaG1qcmFhazh6In0.ytVFjAsRLDJra61yH0ZT-w'
+>>>>>>> parent of f2a7bf3 (remove)
     this.map = new mapboxgl.Map({
       container: 'map', // container ID
       style: 'mapbox://styles/mapbox/satellite-v9', // style URL
       center: [-118.1109043, 33.7827241], // starting position [lng, lat]
       zoom: 14 // starting zoom
     })
+<<<<<<< HEAD
     const start = [-118.1109043, 33.7827241]
     async function getRoute (end) {
       const start = [-118.1109043, 33.7827241]
@@ -159,6 +164,100 @@ export default {
         'cricle-color': '#3887be'
       }
     })
+=======
+    //    const start = [-118.1109043, 33.7827241]
+    //    async function getRoute (end) {
+    //    const query = await fetch(`https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+    //      { method: 'GET' })
+
+  //    const json = await query.json()
+  //    const data = json.routes[0]
+  //    const route = data.geometry.coordinates
+  //    const geojson = { type: 'Feature', properties: {}, geometry: { type: 'LineString', coordinates: route } }
+  //    if (map.getSource('route')) {
+  //      map.getSource('route').setData(geojson)
+  //    } else {
+  //      map.addLayer({
+  //        id: 'route',
+  //        type: 'line',
+  //        source: {
+  //          type: 'geojson', data: geojson
+  //        },
+  //        layout: { 'line-join': 'round', 'line-cap': 'round' },
+  //        paint: { 'line-color': '#3887be', 'line-width': 5, 'line-opacity': 0.75 }
+  //      })
+  //    }
+  //  }
+  //  map.on('load', () => {
+  //    getRoute(start)
+  //    map.addLayer({
+  //      id: 'point',
+  //      type: 'circle',
+  //      source: {
+  //        type: 'geojson',
+  //        data: {
+  //          type: 'FeatureCollection',
+  //          features: [{
+  //            type: 'Feature',
+  //            properties: {},
+  //            geometry: {
+  //              type: 'Point',
+  //              coordinates: start
+  //            }
+  //          }]
+  //        }
+  //      },
+  //      paint: {
+  //        'circle-radius': 10,
+  //        'circle-color': '#3887be'
+  //      }
+  //    })
+  //  }
+  //  )
+  //  map.on('click', (event) => {
+  //    const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key])
+  //    const end = {
+  //      type: 'FeatureCollection',
+  //      features: [{
+  //        type: 'Feature',
+  //        properties: {},
+  //        geometry: {
+  //          type: 'Point',
+  //          coordinates: coords
+  //        }
+  //      }]
+  //    }
+  //    if (map.getLayer('end')) {
+  //      map.getSource('end').setData(end)
+  //    } else {
+  //      map.addLayer({
+  //        id: 'end',
+  //        type: 'circle',
+  //        source: {
+  //          type: 'geojson',
+  //          data: {
+  //            type: 'FeatureCollection',
+  //            features: [
+  //              {
+  //                type: 'Feature',
+  //                properties: {},
+  //                geometry: {
+  //                  type: 'Point',
+  //                  coordinates: coords
+  //                }
+  //              }
+  //            ]
+  //          }
+  //        },
+  //        paint: {
+  //          'circle-radius': 10,
+  //          'circle-color': '#f30'
+  //        }
+  //      })
+  //    }
+  //    getRoute(coords)
+  //  })
+>>>>>>> parent of f2a7bf3 (remove)
   }
 }
 </script>
