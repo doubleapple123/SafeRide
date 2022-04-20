b4deca38aa2f4d554d6c214863ad30909112c301
cantdrinkmilk <53961561+cantdrinkmilk@users.noreply.github.com>
2022-04-20 03:24:41 -0700

    commit
    geocoder

--- SafeRide/Frontend/src/components/MainMap.vue
+++ SafeRide/Frontend/src/components/MainMap.vue
@@ -1,135 +1,71 @@
 <template>
-  <MapHeader></MapHeader>
-  <MapSearchRectangle id="MapSearchRec"></MapSearchRectangle>
-  <div id='map'></div>
-  <MapFooter @selectedOverlayColor="onOverlayColorChange" @selectedDimFooter="onReceiveOverlay"></MapFooter>
+  <div id="mapSearchBox">
+    <form>
+      <input v-model="startLocation" type="text" id="startLocation" placeholder="Start Location"><br>
+      <input v-model="destinationLocation" v-on:keyup.enter="getLocation" type="text" id="destinationLocation" placeholder="Destination Location"><br>
+      <input type="submit" hidden />
+    </form>
+  </div>
 </template>
 
 <script>
-import MapSearchRectangle from '@/components/MapSearchRectangle'
-import MapHeader from '@/components/MapHeader.vue'
-import MapFooter from '@/components/MapFooter'
-import 'mapbox-gl/dist/mapbox-gl.css'
+import axios from 'axios'
 import mapboxgl from 'mapbox-gl'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 export default {
-  components: {
-    MapSearchRectangle,
-    MapFooter,
-    MapHeader
+  data () {
+    return {
+      loading: false,
+      location: '',
+      center: [-118.1109043, 33.7827241],
+      map: {}
+    }
+  },
+  name: 'MapSearchRectangle',
+  methods: {
+    async getLocation () {
+      this.loading = true
+      const resp = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${this.center[0]},${this.center[1]}.json?access_token=${this.api_key}`
+      )
+      this.loading = false
+      this.location = resp.data.features[0].place_name
+    },
+    onSearchEnter: async function () {
+    }
   },
   props: ['api_key'],
   mounted () {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY2FudGRyaW5rbWlsayIsImEiOiJjbDAwZnFiOHkwM3kyM3FwaG1qcmFhazh6In0.ytVFjAsRLDJra61yH0ZT-w'
-    const map = new mapboxgl.Map({
+    mapboxgl.accessToken = this.api_key
+    this.map = new mapboxgl.Map({
       container: 'map', // container ID
-      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      style: 'mapbox://styles/mapbox/satellite-v9', // style URL
       center: [-118.1109043, 33.7827241], // starting position [lng, lat]
       zoom: 14 // starting zoom
     })
-    const start = [-118.1109043, 33.7827241]
-    async function getRoute (end) {
-      const query = await fetch(`https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
-        { method: 'GET' })
+    const geocoder = new MapboxGeocoder({
+      accessToken: this.api_key,
+      mapboxgl: mapboxgl,
+      marker: false
+    })
+    this.map.addControl(geocoder)
 
-      const json = await query.json()
-      const data = json.routes[0]
-      const route = data.geometry.coordinates
-      const geojson = { type: 'Feature', properties: {}, geometry: { type: 'LineString', coordinates: route } }
-      if (map.getSource('route')) {
-        map.getSource('route').setData(geojson)
-      } else {
-        map.addLayer({
-          id: 'route',
-          type: 'line',
-          source: {
-            type: 'geojson', data: geojson
-          },
-          layout: { 'line-join': 'round', 'line-cap': 'round' },
-          paint: { 'line-color': '#3887be', 'line-width': 5, 'line-opacity': 0.75 }
-        })
-      }
-    }
-    map.on('load', () => {
-      getRoute(start)
-      map.addLayer({
-        id: 'point',
-        type: 'circle',
-        source: {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: [{
-              type: 'Feature',
-              properties: {},
-              geometry: {
-                type: 'Point',
-                coordinates: start
-              }
-            }]
-          }
-        },
-        paint: {
-          'circle-radius': 10,
-          'circle-color': '#3887be'
-        }
+    geocoder.on('result', (e) => {
+      const marker = new mapboxgl.Marker({
+        draggable: true,
+        color: '#D80739'
       })
-    }
-    )
-    map.on('click', (event) => {
-      const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key])
-      const end = {
-        type: 'FeatureCollection',
-        features: [{
-          type: 'Feature',
-          properties: {},
-          geometry: {
-            type: 'Point',
-            coordinates: coords
-          }
-        }]
-      }
-      if (map.getLayer('end')) {
-        map.getSource('end').setData(end)
-      } else {
-        map.addLayer({
-          id: 'end',
-          type: 'circle',
-          source: {
-            type: 'geojson',
-            data: {
-              type: 'FeatureCollection',
-              features: [
-                {
-                  type: 'Feature',
-                  properties: {},
-                  geometry: {
-                    type: 'Point',
-                    coordinates: coords
-                  }
-                }
-              ]
-            }
-          },
-          paint: {
-            'circle-radius': 10,
-            'circle-color': '#f30'
-          }
-        })
-      }
-      getRoute(coords)
+        .setLngLat(e.result.center)
+        .addTo(this.map)
+      this.center = e.result.center
+      marker.on('dragend', (e) => {
+        this.center = Object.values(e.target.getLngLat())
+      })
     })
   }
 }
 </script>
 
 <style scoped>
-#map{
-  margin: auto;
-  width: 70%;
-  height: 600px;
-}
-#MapSearchRec{
-  position:fixed;
-  left:50px;
-}
 </style>
