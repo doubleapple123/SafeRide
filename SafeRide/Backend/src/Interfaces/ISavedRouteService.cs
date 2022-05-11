2a5f2724ae87c3469568d64254c904dad3a6e004
cantdrinkmilk <53961561+cantdrinkmilk@users.noreply.github.com>
2022-05-11 02:52:18 -0700

    \
    fix

--- SafeRide/Backend/src/Interfaces/ISavedRouteService.cs
+++ SafeRide/Backend/src/Interfaces/ISavedRouteService.cs
@@ -1,4 +1,4 @@
-﻿
+﻿ 
 namespace SafeRide.src.Services
 {
     public interface ISavedRouteService
@@ -6,6 +6,6 @@ namespace SafeRide.src.Services
         public int AddSavedRoute(string UserID, int routeId, string route, string tableName);
         public List<string> GetRecentRoutes(string userId, string tableName);
         public List<string> GetSavedRoutes(string userId, string tableName);    
-        public Task<string> GetRouteData(string routeUrl, string tableName);
+        public Task<string> GetRouteData(string routeUrl);
     }
 }
